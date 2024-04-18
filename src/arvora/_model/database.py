58d02b73009edc27682575e606f861f2fe28bf13
Fernanda-a <70150556+Fernanda-a@users.noreply.github.com>
2024-04-18 02:05:01 -0300

    Update database.py - adding login and signup logics

--- src/arvora/_model/database.py
+++ src/arvora/_model/database.py
@@ -1,34 +1,66 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
+from tinydb.operations import delete
 import os
 import json
 
 LOCAL_FOLDER = (os.path.abspath(os.path.join(os.path.dirname(__file__))))
 db = TinyDB(os.path.join(LOCAL_FOLDER, 'brain.json'))
 
-class Database:
-    @classmethod
-    def load_articles(cls):
-        return db.all()
+# class Database:
+#     @classmethod
+#     def get(cls):
+#         return db.all()
+#     @classmethod
+#     def insert(cls, content):
+#         for data in content:
+#             article = {
+#                 "title": data.title,
+#                 "autor": data.author,
+#                 "body": data.body,
+#                 "published": data.published
+#             }
+#             db.insert(article)
+
+#     @classmethod
+#     def update(cls):
+#         pass
+
+#     @classmethod
+#     def delete(cls):
+#         pass
+
+class LoginAndSignUp:
 
     @classmethod
-    def insert(cls, content):
-        for data in content:
-            article = {
-                "title": data.title,
-                "autor": data.author,
-                "body": data.body,
-                "published": data.published
+    def login(cls, form):
+        email = form.email
+        password = form.password
+        User = Query()
+
+        if db.search(User.email == email) and db.search(User.password == password):
+            print("Usuário válido. Pode logar!")
+
+        elif not db.search(User.passoword == password):
+            print("Senha incorreta")
+
+        elif not db.search(User.email.exists()):
+            print("Usuário não cadastrado. Cadastre-se!")
+
+
+    @classmethod
+    def sign_up(cls, form):
+        User = Query()
+
+        if not db.search(User.email.exists()):
+            new_user = {
+                "email": form.email,
+                "password": form.password,
+                "phone": form.phone,
+                "name": form.name
             }
-            db.insert(article)
 
-    @classmethod
-    def update(cls):
-        pass
+            db.insert(new_user)
+            print("Usuário cadastrado!")
 
-    @classmethod
-    def delete(cls):
-        pass
-
-
-
-
+        else:
+            ("Usuário já existente")
